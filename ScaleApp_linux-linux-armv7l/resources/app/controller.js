4a64f0232f5421334fafddb5c03ea3e7423498a2
Mostapha Rammo <Mostapha@MacBook-Pro-4.local>
2016-10-06 00:17:14 -0400

    linux app

---
+++ ScaleApp_linux-linux-armv7l/resources/app/controller.js
@@ -0,0 +1,38 @@
+//controller.js
+console.log('in file controller.js');
+
+var App = angular.module('App', []);
+
+App.controller('AppCtrl', function ($scope){
+	
+	$scope.data = '';
+
+	$scope.getPythonData = function(){
+
+		var PythonShell = require('python-shell');
+		var options = {
+			mode: 'text',
+			//pythonPath: '/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python27.zip',
+			scriptPath: __dirname 
+		}
+
+		PythonShell.run('serial.py', options, function(err, results){
+			if (err) throw err;
+			$scope.data = results[0];
+		
+		});
+
+		/*fs = require('fs');
+
+		fs.readFile('~/Desktop/projects/electron-test/hello.txt', 'utf8', function (err,data) {
+  			if (err) {
+    			return console.log(err);
+  			}
+  			console.log(data);
+		});*/
+
+	};
+
+	$scope.getPythonData();
+        
+});
